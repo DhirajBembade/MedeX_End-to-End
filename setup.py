cd02fd4e0296ce28d74f84fb6c09b2c31a03a057
Dhiraj Bembade <dhiraj.bembade@gmail.com>
2026-04-05 18:51:46 +0530

    added CI/CD pipeline

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medex-chatbot",
+    version= "0.1.0",
+    author= "Dhiraj Bembade",
+    author_email="dhiraj.bembade@gmail.com",
+    packages= find_packages(),
+    install_requires=[]
+)
